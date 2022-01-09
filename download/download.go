4159b8d1cb04f4cfd9652b2393d86ae270efe0bd
Jerry <calljerryli@outlook.com>
2022-01-09 15:23:49 +0800

    下载重试机制

--- download/download.go
+++ download/download.go
@@ -1,73 +1,75 @@
 package download
 
 import (
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
-var downloadDir = "./images"
+const BasePath = "images"
 
 // Manager 下载管理器
-// Tasks 任务列表
+// UrlList 任务列表
 // taskCompleteChan 任务完成信道
 type Manager struct {
-	Tasks []string
+	UrlList []string
 }
 
 func init() {
 	// 初始化目录
-	_ = os.Mkdir(downloadDir, 0777)
+	_ = os.Mkdir(BasePath, 0777)
 }
 
 // NewManager 构造一个下载管理器
 func NewManager(tasks []string) (manager *Manager) {
 	manager = &Manager{
-		Tasks: tasks,
+		UrlList: tasks,
 	}
 	return
 }
 
 // Start 开始任务
 func (manager *Manager) Start() {
-	taskNumber := len(manager.Tasks)
-	taskCompleteNumber := 0
-	for _, url := range manager.Tasks {
+	taskCount := len(manager.UrlList)
+	taskCompleteCount := 0
+	for _, url := range manager.UrlList {
 		log.Printf(":任务开始:%s\n", url)
 		manager.download(url)
-		taskCompleteNumber += 1
-		log.Printf(":任务完成:%d/%d:%s\n", taskCompleteNumber, taskNumber, url)
+		taskCompleteCount += 1
+		log.Printf(":任务完成:%d/%d:%s\n", taskCompleteCount, taskCount, url)
 	}
 }
 
+// download 下载
 func (manager *Manager) download(url string) {
-	res, err := http.Get(url)
-	checkError(err)
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(res.Body)
-
-	fileNameSplit := strings.Split(url, "/")
-	fileName := fileNameSplit[len(fileNameSplit)-1]
-	file, err := os.Create(downloadDir + "/" + fileName)
-	checkError(err)
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-
-	buf := make([]byte, 100*1024)
 	for {
-		n, err := res.Body.Read(buf)
-		if n == 0 {
-			break
-		}
-		if err != io.EOF {
-			checkError(err)
+		response, err := http.Get(url)
+		checkError(err)
+
+		responseStatusCode := response.StatusCode
+		log.Println(":download():", ":url:", url, ":responseStatusCode:", responseStatusCode)
+		if responseStatusCode != 200 {
+			log.Println(":download():", ":url:", url, "retry...")
+			time.Sleep(time.Second * 2)
+			continue
 		}
 
-		_, _ = file.Write(buf[:n])
+		urlSplit := strings.Split(url, "/")
+		filename := urlSplit[len(urlSplit)-1]
+
+		file, err := os.Create(BasePath + "/" + filename)
+		checkError(err)
+
+		bytes, _ := ioutil.ReadAll(response.Body)
+		_, _ = file.Write(bytes)
+
+		_ = response.Body.Close()
+		_ = file.Close()
+
+		break
 	}
 }
 
--- spider/spider.go
+++ spider/spider.go
@@ -25,30 +25,27 @@ func Get24NImage() {
 	for {
 		fmt.Print("即将获取 24*N 张图片，请输入 N\n>>> ")
 		_, err := fmt.Scanf("%d", &n)
-		if err != nil {
-			checkError(err)
+		checkError(err)
+		if n > 0 {
+			break
 		}
-		if n <= 0 {
-			fmt.Println("N 必须大于 0")
-			continue
-		}
-		break
+		fmt.Println("N 必须大于 0")
 	}
-
 	for i := 0; i < n; i++ {
 		log.Printf("开始第 %d 次任务\n", i+1)
 		Get24Image()
 	}
 }
 
-func getDoc(url string) *goquery.Document {
+func getDocByUrl(url string) *goquery.Document {
 	for {
 		response, err := http.Get(url)
 		checkError(err)
 
 		responseStatusCode := response.StatusCode
-		log.Println(":getDoc():", ":url:", url, ":responseStatusCode:", responseStatusCode)
+		log.Println(":getDocByUrl():", ":url:", url, ":responseStatusCode:", responseStatusCode)
 		if responseStatusCode != 200 {
+			log.Println(":getDocByUrl():", ":url:", url, "retry...")
 			time.Sleep(time.Second * 2)
 			continue
 		}
@@ -62,25 +59,24 @@ func getDoc(url string) *goquery.Document {
 	}
 }
 
-func getPageUrlList() (hrefSlice []string) {
-	doc := getDoc("https://wallhaven.cc/random")
+func getPageUrlList() []string {
+	pageUrlList := make([]string, 0)
+	doc := getDocByUrl("https://wallhaven.cc/random")
 	doc.Find("a.preview").Each(func(i int, s *goquery.Selection) {
-		href, exists := s.Attr("href")
+		pageUrl, exists := s.Attr("href")
 		if !exists {
 			return
 		}
-
-		hrefSlice = append(hrefSlice, href)
+		pageUrlList = append(pageUrlList, pageUrl)
 	})
-	return
+	return pageUrlList
 }
 
-func getImageUrlByPageUrl(pageUrl string) (imageUrl string) {
-	doc := getDoc(pageUrl)
+func getImageUrlByPageUrl(pageUrl string) string {
+	doc := getDocByUrl(pageUrl)
 	element := doc.Find("#wallpaper")
-	imageUrl, _ = element.Attr("src")
-	log.Println(":getImageUrlByPageUrl():", ":pageUrl:", pageUrl, ":imageUrl:", imageUrl)
-	return
+	imageUrl, _ := element.Attr("src")
+	return imageUrl
 }
 
 func getImageUrlList() []string {
